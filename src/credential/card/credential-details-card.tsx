322e7d24cf8d826642d17396809cf5f374253efb
Developer <info@procivis.ch>
2024-04-10 09:55:22 +0300

    Only show preview attributes, include button to show all

--- src/credential/card/credential-details-card.tsx
+++ src/credential/card/credential-details-card.tsx
@@ -10,6 +10,7 @@ import {
   ViewStyle,
 } from 'react-native';
 
+import { Button, ButtonType } from '../../buttons';
 import { DownIcon, UpIcon } from '../../icons/credential';
 import { useAppColorScheme } from '../../theme/color-scheme-context';
 import { concatTestID } from '../../utils/testID';
@@ -24,7 +25,10 @@ export type CredentialDetailsCardProps = {
   onImagePreview?: (name: string, image: ImageSourcePropType) => void;
   style?: StyleProp<ViewStyle>;
   testID?: string;
+  showAllButtonLabel?: string;
 };
+
+const PREVIEW_ATTRIBUTES_COUNT = 3;
 
 const CredentialDetailsCard: FC<CredentialDetailsCardProps> = ({
   attributes,
@@ -34,33 +38,78 @@ const CredentialDetailsCard: FC<CredentialDetailsCardProps> = ({
   onImagePreview,
   style,
   testID,
+  showAllButtonLabel = 'See all',
 }) => {
   const colorScheme = useAppColorScheme();
-  const [animation] = useState(() => new Animated.Value(expanded ? 1 : 0));
-  const [attributesHeight, setAttributesHeight] = useState<number>();
+  const [expandCardAnimation] = useState(() => new Animated.Value(expanded ? 1 : 0));
+  const [showAllAttributesAnimation] = useState(() => new Animated.Value(0));
+
+  const [previewAttributesHeight, setPreviewAttributesHeight] = useState<number>(0);
+  const [fullAttributesHeight, setFullAttributesHeight] = useState<number>(0);
+  const [buttonViewHeight, setButtonViewHeight] = useState<number>();
+
+  const [showAllAttributes, setShowAllAttributes] = useState<boolean>(false);
+
   const CaretIcon = expanded ? UpIcon : DownIcon;
 
   useEffect(() => {
-    Animated.timing(animation, {
+    Animated.timing(expandCardAnimation, {
       duration: 250,
       easing: Easing.quad,
       toValue: expanded ? 1 : 0,
       useNativeDriver: false,
     }).start();
-  }, [expanded, animation]);
+  }, [expanded, expandCardAnimation]);
+
+  useEffect(() => {
+    Animated.timing(showAllAttributesAnimation, {
+      duration: 200,
+      easing: Easing.quad,
+      toValue: expanded && showAllAttributes ? 1 : 0,
+      useNativeDriver: false,
+    }).start();
+  }, [showAllAttributes, showAllAttributesAnimation, expanded]);
 
   const attributesWrapperStyle: Animated.WithAnimatedObject<ViewStyle> = {
-    height: attributesHeight
-      ? animation.interpolate({
-          extrapolate: 'clamp',
-          inputRange: [0, 1],
-          outputRange: [0, attributesHeight],
-        })
-      : undefined,
+    height:
+      previewAttributesHeight &&
+      expandCardAnimation.interpolate({
+        extrapolate: 'clamp',
+        inputRange: [0, 1],
+        outputRange: [0, previewAttributesHeight],
+      }),
   };
 
-  const onContentLayout = useCallback((event: LayoutChangeEvent) => {
-    setAttributesHeight(event.nativeEvent.layout.height);
+  const fullAttributesWrapperStyle: Animated.WithAnimatedObject<ViewStyle> = {
+    height:
+      fullAttributesHeight &&
+      showAllAttributesAnimation.interpolate({
+        extrapolate: 'clamp',
+        inputRange: [0, 1],
+        outputRange: [0, fullAttributesHeight],
+      }),
+  };
+
+  const buttonWrapperStyle: Animated.WithAnimatedObject<ViewStyle> = {
+    height:
+      buttonViewHeight &&
+      showAllAttributesAnimation.interpolate({
+        extrapolate: 'clamp',
+        inputRange: [0, 1],
+        outputRange: [buttonViewHeight, 0],
+      }),
+  };
+
+  const onPreviewAttrContentLayout = useCallback((event: LayoutChangeEvent) => {
+    setPreviewAttributesHeight(event.nativeEvent.layout.height);
+  }, []);
+
+  const onFullAttributesContentLayout = useCallback((event: LayoutChangeEvent) => {
+    setFullAttributesHeight(event.nativeEvent.layout.height);
+  }, []);
+
+  const onButtonViewLayout = useCallback((event: LayoutChangeEvent) => {
+    setButtonViewHeight(event.nativeEvent.layout.height);
   }, []);
 
   const footerView: ReactElement | undefined = useMemo(() => {
@@ -77,6 +126,9 @@ const CredentialDetailsCard: FC<CredentialDetailsCardProps> = ({
 
   const { header, ...cardProps } = card;
 
+  const previewAttributes = attributes.slice(0, PREVIEW_ATTRIBUTES_COUNT);
+  const extraAttributes = attributes.slice(PREVIEW_ATTRIBUTES_COUNT);
+
   return (
     <View style={[styles.detailsCard, { backgroundColor: colorScheme.white }, style]} testID={testID}>
       <CredentialCard
@@ -86,11 +138,29 @@ const CredentialDetailsCard: FC<CredentialDetailsCardProps> = ({
         testID={concatTestID(testID, 'card')}
       />
       <Animated.View style={[styles.attributesAnimatedWrapper, attributesWrapperStyle]}>
-        <View onLayout={onContentLayout} style={styles.attributesWrapper}>
-          {attributes.map((attribute, index, { length }) => (
+        <View onLayout={onPreviewAttrContentLayout} style={styles.attributesWrapper}>
+          {previewAttributes.map((attribute) => (
+            <CredentialAttributeItem key={attribute.id} last={false} onImagePreview={onImagePreview} {...attribute} />
+          ))}
+          <Animated.View style={[styles.attributesAnimatedWrapper, buttonWrapperStyle]}>
+            <View onLayout={onButtonViewLayout}>
+              <Button
+                onPress={() => setShowAllAttributes(true)}
+                type={ButtonType.Secondary}
+                testID={concatTestID(testID, 'show-all-attributes-button')}
+                title={showAllButtonLabel}
+              />
+            </View>
+          </Animated.View>
+        </View>
+      </Animated.View>
+      <Animated.View style={[styles.attributesAnimatedWrapper, fullAttributesWrapperStyle]}>
+        <View onLayout={onFullAttributesContentLayout} style={styles.attributesWrapper}>
+          {extraAttributes.map((attribute, index, { length }) => (
             <CredentialAttributeItem
               key={attribute.id}
               last={!footerView && index === length - 1}
+              testID={concatTestID(testID, 'attribute', attribute.id)}
               onImagePreview={onImagePreview}
               {...attribute}
             />
