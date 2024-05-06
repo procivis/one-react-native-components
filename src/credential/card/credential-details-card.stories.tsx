49ab5ae266ddf1a48540882f110e88716522d304
Łukasz Przytuła <lprzytula@gmail.com>
2024-05-06 17:31:58 +0200

    Fixed credential details card attributes to allow selection

--- src/credential/card/credential-details-card.stories.tsx
+++ src/credential/card/credential-details-card.stories.tsx
@@ -6,14 +6,18 @@ import { Text } from 'react-native-svg';
 import CredentialDetailsCardBackground from '../../../storybook/assets/CredentialCardBackground.png';
 import { Placeholder } from '../../../storybook/placeholder';
 import { AlertOutlineIcon, CredentialWarningIcon } from '../../icons/credential';
+import Selector, { SelectorStatus } from '../selector';
 import CredentialDetailsCard from './credential-details-card';
 import { CarouselImageType } from './credential-image-carousel';
 
 const Basic: ComponentStory<typeof CredentialDetailsCard> = ({ ...args }) => {
-  return <CredentialDetailsCard {...args} />;
+  return <CredentialDetailsCard {...args} style={style.card} />;
 };
 
 const style = StyleSheet.create({
+  card: {
+    margin: 16,
+  },
   carouselItem: {
     flex: 1,
   },
@@ -54,26 +58,33 @@ Basic.args = {
       image: {
         uri: 'https://cdn.pixabay.com/photo/2013/07/12/17/47/test-pattern-152459_1280.png',
       },
+      rightAccessory: <Selector status={SelectorStatus.Required} />,
     },
     {
+      selected: true,
       id: 'attribute-3',
       name: 'Attribute 3',
       value: 'Value 3',
+      rightAccessory: <Selector status={true ? SelectorStatus.SelectedCheckmark : SelectorStatus.Empty} />,
     },
     {
       id: 'attribute-4',
       name: 'Attribute 4',
       value: 'Value 4',
+      rightAccessory: <Selector status={false ? SelectorStatus.SelectedCheckmark : SelectorStatus.Empty} />,
     },
     {
+      selected: true,
       id: 'attribute-5',
       name: 'Attribute 5',
       value: 'Value 5',
+      rightAccessory: <Selector status={true ? SelectorStatus.SelectedCheckmark : SelectorStatus.Empty} />,
     },
     {
       id: 'attribute-6',
       name: 'Attribute 5',
       value: 'Value 5',
+      rightAccessory: <Selector status={false ? SelectorStatus.SelectedCheckmark : SelectorStatus.Empty} />,
     },
   ],
   card: {
@@ -122,6 +133,7 @@ Basic.args = {
       <Text>Footer view</Text>
     </View>
   ),
+  onAttributeSelected: console.log,
 };
 
 export { Basic as CredentialDetailsCard };
@@ -130,6 +142,9 @@ export default {
   title: 'credential/card/CredentialDetailsCard',
   component: CredentialDetailsCard,
   parameters: {
+    backgrounds: {
+      default: 'background',
+    },
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/52qDYWUMjXAGre1dcnz5bz/Procivis-One-Wallet?type=design&node-id=415-10041&mode=design&t=0DWIGZWyWYB7JzPE-0',
