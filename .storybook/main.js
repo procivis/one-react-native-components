2b2e5225d17f43e39d16da6851bda33672afa898
Łukasz Przytuła <lprzytula@gmail.com>
2024-07-23 18:39:42 +0200

    Extracted qr code scanner

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,6 +21,7 @@ module.exports = {
         '../../Utilities/Platform': 'react-native-web/src/exports/Platform',
         '@react-native-community/blur': '/storybook/modules/blur',
         '@procivis/react-native-picker': 'react-native-web/src/exports/View',
+        'react-native-vision-camera': '/storybook/modules/react-native-vision-camera',
       },
     },
   }),
