d9a4e7e6d0d2865351254e6d0bf94c3853c95f40
Nicu Sandu <sandu@procivis.ch>
2024-09-19 11:46:52 +0200

    [ONE-3411] Merge react-native-components for wallet

---
+++ src/action-modal/ActionModal.stories.tsx
@@ -0,0 +1,35 @@
+import type { ComponentMeta, Story } from '@storybook/react';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+
+import { Button } from '../buttons';
+import { Typography } from '../text';
+import ActionModal from './ActionModal';
+
+const Basic: Story = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <>
+      <Button title="Show modal" onPress={() => setVisible(true)} />
+      <ActionModal visible={visible}>
+        <View>
+          <Typography color={'white'}>Example text in modal</Typography>
+          <Button title="Hide modal" onPress={() => setVisible(false)} />
+        </View>
+      </ActionModal>
+    </>
+  );
+};
+
+export { Basic as ActionModal };
+
+export default {
+  title: 'base/Action Modal',
+  component: ActionModal,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/Gd0Tj0234hxtl3HMcCJThW/App-Component-Library-(Design)?node-id=391-9097&t=WhKthlRU5MCuUWfw-0',
+    },
+  },
+} as ComponentMeta<typeof ActionModal>;
