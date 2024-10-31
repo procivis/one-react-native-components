3f167c68b5d7052c369792fe7d1a418c1a648ca1
Developer <info@procivis.ch>
2024-10-31 11:32:26 +0100

    Added missing CredentialCardNotice export

--- src/ui-components/credential/card/index.ts
+++ src/ui-components/credential/card/index.ts
@@ -1,11 +1,16 @@
-import CredentialCard, { CredentialCardProps, CredentialCardRatio, CredentialCardShadow } from './credential-card';
+import CredentialCard, {
+  CredentialCardNotice,
+  CredentialCardProps,
+  CredentialCardRatio,
+  CredentialCardShadow,
+} from './credential-card';
 import CredentialDetailsCard, { CredentialDetailsCardProps } from './credential-details-card';
 import CredentialDetailsCardListItem, { CredentialDetailsCardListItemProps } from './credential-details-list-item';
 import CredentialHeader, { CredentialHeaderProps } from './credential-header';
 import { CarouselImage, CarouselImageType } from './credential-image-carousel';
 
 export { CarouselImage, CarouselImageType };
-export { CredentialCard, CredentialCardProps, CredentialCardRatio, CredentialCardShadow };
+export { CredentialCard, CredentialCardNotice, CredentialCardProps, CredentialCardRatio, CredentialCardShadow };
 export { CredentialDetailsCard, CredentialDetailsCardProps };
 export { CredentialDetailsCardListItem, CredentialDetailsCardListItemProps };
 export { CredentialHeader, CredentialHeaderProps };
