ea4d4c85482f47e2bd20ec2473308479452431c0
Developer <info@procivis.ch>
2025-01-28 12:10:10 +0100

    Added missing exports for Pin Lock Modal

--- src/ui-components/pin/index.ts
+++ src/ui-components/pin/index.ts
@@ -1,4 +1,5 @@
 import { Biometry } from './keypad';
 import { PinCodeScreen, PinCodeScreenActions, PinCodeScreenProps } from './pin-code-screen';
+import PinLockModal, { PinLockModalLabels } from './pin-lock';
 
-export { Biometry, PinCodeScreen, PinCodeScreenActions, PinCodeScreenProps };
+export { Biometry, PinCodeScreen, PinCodeScreenActions, PinCodeScreenProps, PinLockModal, PinLockModalLabels };
