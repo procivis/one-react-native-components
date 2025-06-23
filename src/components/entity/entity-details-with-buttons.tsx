bd81968189ed7bec6c5b48aa8c8baf1f0db25482
Developer <info@procivis.ch>
2025-06-23 15:14:11 +0200

    Updates for trust information display

--- src/components/entity/entity-details-with-buttons.tsx
+++ src/components/entity/entity-details-with-buttons.tsx
@@ -1,14 +1,13 @@
 import { IdentifierListItem, TrustEntityRoleEnum, TrustEntityStateEnum } from '@procivis/react-native-one-core';
-import React, { FC, memo, useMemo } from 'react';
+import React, { FC, memo } from 'react';
 import { StyleProp, View, ViewStyle } from 'react-native';
 
-import EntityCluster from '../../ui-components/entity/entity-cluster';
-import { EntityNotTrustedIcon, EntityTrustedIcon } from '../../ui-components/icons';
 import { AttributesLabels, EntityLabels, EntityType } from '../../ui-components/screens/nerd-mode-screen';
 import { useAppColorScheme } from '../../ui-components/theme/color-scheme-context';
-import { concatTestID, replaceBreakingHyphens, useIdentifierDetails } from '../../utils';
+import { concatTestID, useIdentifierDetails } from '../../utils';
 import { useTrustEntity } from '../../utils/hooks/core/trust-entity';
 import EntityAttributes from './entity-attributes';
+import EntityDetails from './entity-details';
 import EntityButtons from './EntityButtons';
 
 export type ContextRole = Exclude<TrustEntityRoleEnum, TrustEntityRoleEnum.BOTH>;
@@ -47,47 +46,14 @@ const EntityDetailsWithButtons: FC<EntityDetailsWithButtonsProps> = ({
     (trustEntity.role === TrustEntityRoleEnum.BOTH || trustEntity.role === role),
   );
 
-  const trustEntityName = useMemo(() => {
-    if (!trustEntity || !trusted) {
-      if (identifierDetail?.did) {
-        return replaceBreakingHyphens(identifierDetail.did.did);
-      }
-      if (identifierDetail?.certificates?.length) {
-        return identifierDetail.certificates[0].name;
-      }
-
-      return role === TrustEntityRoleEnum.ISSUER ? entityLabels.unknownIssuer : entityLabels.unknownVerifier;
-    }
-    return trustEntity.name;
-  }, [entityLabels, role, trustEntity, trusted, identifierDetail]);
-
-  const trustEntitySubline = useMemo(() => {
-    if (!trusted) {
-      return undefined;
-    }
-
-    return `${entityLabels.trusted} • ${trustEntity?.trustAnchor.name}`;
-  }, [trustEntity, trusted, entityLabels.trusted]);
-
-  const trustEntityStatusIcon = useMemo(() => {
-    if (!trusted) {
-      return <EntityNotTrustedIcon testID={concatTestID(testID, 'statusIcon', 'notTrusted')} />;
-    }
-    return <EntityTrustedIcon testID={concatTestID(testID, 'statusIcon', 'trusted')} />;
-  }, [testID, trusted]);
-
   return (
     <View style={{ backgroundColor: colorScheme.nerdView.background }}>
-      <EntityCluster
-        avatar={{
-          avatar: trusted && trustEntity?.logo ? { imageSource: { uri: trustEntity.logo } } : undefined,
-          placeholderText: trusted ? trustEntity?.name.substring(0, 1) : "U",
-          statusIcon: trustEntityStatusIcon,
-          testID: concatTestID(testID, 'avatar'),
+      <EntityDetails
+        identifier={identifier}
+        labels={{
+          unknown: role === TrustEntityRoleEnum.ISSUER ? entityLabels.unknownIssuer : entityLabels.unknownVerifier,
         }}
-        entityName={trustEntityName}
-        subline={trustEntitySubline}
-        sublineColor={trusted ? colorScheme.success : colorScheme.white}
+        role={role}
         testID={testID}
         {...props}
       />
--- src/components/entity/entity-details.tsx
+++ src/components/entity/entity-details.tsx
@@ -1,7 +1,8 @@
-import { IdentifierListItem,TrustEntity, TrustEntityRoleEnum, TrustEntityStateEnum } from '@procivis/react-native-one-core';
+import { IdentifierCertificateDetail, IdentifierListItem,TrustEntity, TrustEntityRoleEnum, TrustEntityStateEnum } from '@procivis/react-native-one-core';
 import React, { FC, ReactNode, useMemo } from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
 
+import { AvatarProps, LoaderView } from '../../ui-components';
 import EntityCluster from '../../ui-components/entity/entity-cluster';
 import { EntityNotTrustedIcon, EntityTrustedIcon } from '../../ui-components/icons';
 import { concatTestID, replaceBreakingHyphens, useIdentifierDetails } from '../../utils';
@@ -16,6 +17,8 @@ export type EntityDetailsProps = {
   renderMore?: (trustEntity: TrustEntity) => ReactNode;
   role: Exclude<TrustEntityRoleEnum, TrustEntityRoleEnum.BOTH>;
   style?: StyleProp<ViewStyle>;
+  sublineColor?: string;
+  textColor?: string;
   testID?: string;
 } & (
     | {
@@ -26,52 +29,83 @@ export type EntityDetailsProps = {
     }
   );
 
-const EntityDetails: FC<EntityDetailsProps> = ({ labels, renderMore, role, style, testID, ...props }) => {
-  const { data } = useTrustEntity('identifier' in props ? props.identifier?.id : undefined);
+const getCertificateCommonName = (certificate: IdentifierCertificateDetail): string | undefined => {
+  return certificate.x509Attributes.subject.split(', ').find((s) => s.startsWith('CN'))?.split('=')?.pop();
+}
+
+const EntityDetails: FC<EntityDetailsProps> = ({ labels, renderMore, role, style, testID, sublineColor, textColor, ...props }) => {
+  const { data, isLoading: isLoadingTrustEntity } = useTrustEntity('identifier' in props ? props.identifier?.id : undefined);
   const trustEntity: TrustEntity | undefined = 'entity' in props ? props.entity : data ?? undefined;
-  const { data: identifierDetail } = useIdentifierDetails('identifier' in props ? props.identifier?.id : undefined);
+  const { data: identifierDetail, isLoading: isLoadingIdentifier } = useIdentifierDetails('identifier' in props ? props.identifier?.id : undefined);
 
   const trusted =
     trustEntity &&
     trustEntity.state === TrustEntityStateEnum.ACTIVE &&
     (trustEntity.role === TrustEntityRoleEnum.BOTH || trustEntity.role === role);
 
-  const trustEntityName = useMemo(() => {
-    if (!trusted) {
-      if (identifierDetail?.did?.did) {
-        return replaceBreakingHyphens(identifierDetail.did.did);
-      }
-      return labels.unknown;
-    }
-    return trustEntity.name;
-  }, [labels.unknown, trustEntity, trusted, identifierDetail]);
+  const isLoading = isLoadingIdentifier || isLoadingTrustEntity;
 
-  const trustEntityStatusIcon = useMemo(() => {
-    if (!trusted) {
-      return <EntityNotTrustedIcon testID={concatTestID(testID, 'statusIcon', 'notTrusted')} />;
+  const avatarProps: AvatarProps | undefined = useMemo(() => {
+    if (isLoading) {
+      return {
+        avatar: {
+          component: <LoaderView animate={true} />
+        }
+      }
     }
-    return <EntityTrustedIcon testID={concatTestID(testID, 'statusIcon', 'trusted')} />;
-  }, [testID, trusted]);
+
+    const avatar = trusted && trustEntity.logo ? { imageSource: { uri: trustEntity.logo } } : undefined;
+
+    const placeholderText = identifierDetail?.type === 'CERTIFICATE' && identifierDetail.certificates?.[0] ? getCertificateCommonName(identifierDetail.certificates[0])?.substring(0, 1) : 'D';
+
+    const statusIcon = trusted ? 
+      <EntityTrustedIcon testID={concatTestID(testID, 'statusIcon', 'trusted')} /> :
+      <EntityNotTrustedIcon testID={concatTestID(testID, 'statusIcon', 'notTrusted')} />;
+
+    if (trustEntity) {
+      return {
+        avatar,
+        placeholderText: trusted && identifierDetail?.type === 'DID' ? trustEntity.name.substring(0, 1) : placeholderText,
+        statusIcon,
+        testID: concatTestID(testID, 'avatar'),
+      };
+    }
+
+    return {
+      placeholderText,
+      statusIcon,
+    }
+  }, [trustEntity, trusted, identifierDetail, isLoading, testID]);
+
+  const trustEntityName = useMemo(() => {
+    if (isLoading) {
+      return '';
+    }
+    if (identifierDetail?.type === 'CERTIFICATE' && identifierDetail.certificates?.[0]) {
+      const commonName = getCertificateCommonName(identifierDetail.certificates[0]);
+      if (commonName) {
+        return commonName;
+      }
+    }
+    if (trustEntity) {
+      return trustEntity.name;
+    }
+    if (identifierDetail?.did?.did) {
+      return replaceBreakingHyphens(identifierDetail.did.did);
+    }
+
+    return labels.unknown;
+  }, [labels.unknown, trustEntity, identifierDetail, isLoading]);
 
   return (
     <>
       <EntityCluster
-        avatar={
-          trustEntity
-            ? {
-              avatar: trusted && trustEntity.logo ? { imageSource: { uri: trustEntity.logo } } : undefined,
-              placeholderText: trusted ? trustEntity.name.substring(0, 1) : "U",
-              statusIcon: trustEntityStatusIcon,
-              testID: concatTestID(testID, 'avatar'),
-            }
-            : {
-              statusIcon: trustEntityStatusIcon,
-              placeholderText: "U",
-            }
-        }
+        avatar={avatarProps}
         entityName={trustEntityName}
         style={style}
         testID={testID}
+        textColor={textColor}
+        sublineColor={sublineColor}
       />
       {trustEntity && renderMore && renderMore(trustEntity)}
     </>
