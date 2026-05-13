172b8e6b07723b877fe438da5c1e9dd8c8070d88
Developer <info@procivis.ch>
2026-05-13 17:00:30 +0800

    ONE-9640: trust collection sync update

--- src/utils/hooks/core/trust-list.ts
+++ src/utils/hooks/core/trust-list.ts
@@ -1,29 +1,40 @@
-import { HolderWalletUnit } from '@procivis/react-native-one-core';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 
 import { reportException } from '../../reporting';
 import { useONECore } from './core-context';
+import { VERIFIER_INSTANCE_TRUST_COLLECTIONS_QUERY_KEY } from './verifier-instance';
+import { WALLET_UNIT_TRUST_COLLECTIONS_QUERY_KEY } from './wallet-unit';
 
 export const useTrustCollectionSync = () => {
+  const queryClient = useQueryClient();
   const { core } = useONECore();
 
-  return useMutation(
-    async (params: { holderWalletUnitId?: HolderWalletUnit['id']; verifierInstanceId?: string }) =>
-      core.runTask('TRUST_COLLECTION_SYNC', JSON.stringify(params)),
-    {
-      onError: (err) => {
-        reportException(err, 'TRUST_COLLECTION_SYNC error');
-      },
+  return useMutation(async () => core.runTask('TRUST_COLLECTION_SYNC', undefined), {
+    onError: async (err) => {
+      reportException(err, 'TRUST_COLLECTION_SYNC error');
+      await queryClient.invalidateQueries(WALLET_UNIT_TRUST_COLLECTIONS_QUERY_KEY);
+      await queryClient.invalidateQueries(VERIFIER_INSTANCE_TRUST_COLLECTIONS_QUERY_KEY);
     },
-  );
+    onSuccess: async () => {
+      await queryClient.invalidateQueries(WALLET_UNIT_TRUST_COLLECTIONS_QUERY_KEY);
+      await queryClient.invalidateQueries(VERIFIER_INSTANCE_TRUST_COLLECTIONS_QUERY_KEY);
+    },
+  });
 };
 
 export const useTrustListSubscriptionUpdate = () => {
+  const queryClient = useQueryClient();
   const { core } = useONECore();
 
   return useMutation(async () => core.runTask('TRUST_LIST_SUBSCRIPTION_UPDATE', undefined), {
-    onError: (err) => {
+    onError: async (err) => {
       reportException(err, 'TRUST_LIST_SUBSCRIPTION_UPDATE error');
+      await queryClient.invalidateQueries(WALLET_UNIT_TRUST_COLLECTIONS_QUERY_KEY);
+      await queryClient.invalidateQueries(VERIFIER_INSTANCE_TRUST_COLLECTIONS_QUERY_KEY);
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries(WALLET_UNIT_TRUST_COLLECTIONS_QUERY_KEY);
+      await queryClient.invalidateQueries(VERIFIER_INSTANCE_TRUST_COLLECTIONS_QUERY_KEY);
     },
   });
 };
